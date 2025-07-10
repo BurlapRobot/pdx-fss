962282d45c9a54fa21ad358bf6829049a13df6e4
Kim Robinson <kimmykokonut@gmail.com>
2025-07-10 13:15:23 -0700

    [navbar] use md variable in navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,20 @@
-import React from "react";
 
-const Navbar = ({test}) => (
+const Navbar = ({title, subtitle, links}) => (
   <nav className="bg-black text-yellow-300 px-4 py-2 flex items-center justify-between w-full">
     <div className="flex items-center space-x-2">
-      <span className="font-bold text-sm">SAFE{test}</span>
-      <span className="text-xs">Families for Safe Streets Portland</span>
+      {/* TODO: won't this be a logo/img? */}
+      <span className="font-bold text-sm">{title}</span>
+      <span className="text-xs">{subtitle}</span>
     </div>
     <ul className="flex space-x-6 text-xs">
-      <li><a href="#" className="hover:underline">Safe Streets Now</a></li>
-      <li><a href="#" className="hover:underline">Advocacy</a></li>
-      <li><a href="#" className="hover:underline">Get Involved</a></li>
-      <li><a href="#" className="hover:underline">Services & Support</a></li>
-      <li><a href="#" className="hover:underline">News</a></li>
+      {/* TODO: won't these be dropdowns? */}
+      {links.map((link, index) => (
+        <li key={index}><a href={link.url} className="hover:underline">{link.text}</a></li>
+      ))}
     </ul>
+    {/* TODO: add a search input? */}
     <div className="text-white cursor-pointer">🔍</div>
   </nav>
 );
 
-export default Navbar; 
+export default Navbar;
