31cb479a6304802d7b92f290ed50d30d6110086c
Kim Robinson <kimmykokonut@gmail.com>
2025-07-14 12:42:34 -0700

    [navbar] navbar displays menu items as buttons with dropdown lists, style wip

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,35 @@
+import { useState } from "react";
 
-const Navbar = ({title, subtitle, links}) => (
-  <nav className="bg-black text-yellow-300 px-4 py-2 flex items-center justify-between w-full">
-    <div className="flex items-center space-x-2">
-      {/* TODO: won't this be a logo/img? */}
-      <span className="font-bold text-sm">{title}</span>
-      <span className="text-xs">{subtitle}</span>
-    </div>
-    <ul className="flex space-x-6 text-xs">
-      {/* TODO: won't these be dropdowns? */}
-      {links.map((link, index) => (
-        <li key={index}><a href={link.url} className="hover:underline">{link.text}</a></li>
-      ))}
-    </ul>
-    {/* TODO: add a search input? */}
-    <div className="text-white cursor-pointer">🔍</div>
-  </nav>
-);
+const Navbar = ({logo, title, menu}) => {
+  const [openMenu, setOpenMenu] = useState(null);
+
+  return (
+    <nav className="bg-black text-yellow-300 px-4 py-2 flex items-center justify-between w-full">
+      <div className="flex items-center space-x-2">
+        {/* TODO: add logo */}
+        <span className="font-bold text-sm">{logo}</span>
+        <span className="text-xs">{title}</span>
+      </div>
+
+      <ul className="flex space-x-6 text-xs">
+        {menu.map((menuItem, index) => (
+          <li key={index}>
+            <button onClick={() => setOpenMenu(openMenu === index ? null : index)} className="hover:underline">{menuItem.text}
+            </button>
+            {openMenu === index && (
+              <ul>
+                {menuItem.items.map((link, index) => (
+                  <li key={index}><a href={link.url} className="hover:underline">{link.text}</a></li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
+      {/* TODO: add a search input? */}
+      <div className="text-xs cursor-pointer text-nowrap">Search 🔍</div>
+    </nav>
+  );
+};
 
 export default Navbar;
