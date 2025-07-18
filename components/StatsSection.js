59763264723e60f5fa80641b9b2139206d2446fc
Kim Robinson <kimmykokonut@gmail.com>
2025-07-18 10:39:52 -0700

    [stats] update section to use Image over div and cms to have image picker widget

--- components/StatsSection.js
+++ components/StatsSection.js
@@ -1,22 +1,25 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 
 const StatsSection = ({ image, heading, subheading, text, ctaLink, cta }) => (
   <section className="bg-yellow-300 py-8 px-4 flex flex-col md:flex-row items-center justify-center gap-8">
-    <div className={`
+    <Image
+      src={image}
+      alt='placeholder image'
+      width={256}
+      height={160}
+      className={`
       bg-gray-100
       border
       border-gray-300
-      w-64
-      h-40
       flex
       items-center
       justify-center
       text-gray-500
       text-lg
-      font-semibold`}>
-      {image}
-    </div>
+      font-semibold`}
+    />
     <div className="max-w-lg">
       <h2 className="font-bold text-lg mb-2">{heading}</h2>
       <p className="font-semibold mb-1">{subheading}</p>
