10f0036cb2ba11c28e2a77f094f50fba8e2028f5
Kim Robinson <kimmykokonut@gmail.com>
2025-07-28 12:18:07 -0700

    [action] take action page content can be edited in cms

--- pages/action.js
+++ pages/action.js
@@ -1,33 +1,40 @@
-// import ReactMarkdown from 'react-markdown';
-// import AlertBar from "../components/AlertBar";
-// import Footer from "../components/Footer";
-// import Navbar from "../components/Navbar";
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+import ReactMarkdown from 'react-markdown';
+import AlertBar from "../components/AlertBar";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
-export default function TakeAction() {
+export default function TakeAction({ title, body }) {
   return (
-    <div><h2>Testing take action</h2></div>
-    // <div className="min-h-screen flex flex-col">
-    //   <AlertBar />
-    //   <Navbar />
-    //   <main className="flex-1 bg-white">
-    //     <div className="max-w-4xl mx-auto py-8 px-4 space-y-12">
-    //       <section>
-    //         <h1 className="text-3xl font-bold mb-4">{title}</h1>
-    //         <div className="mb-6 text-lg prose prose-lg">
-    //           <ReactMarkdown>{intro}</ReactMarkdown>
-    //         </div>
-    //       </section>
-    //       {sections && sections.map((section, idx) => (
-    //         <section key={idx}>
-    //           <h2 className="text-2xl font-semibold mb-2">{section.sectionTitle}</h2>
-    //           <div className="prose prose-lg">
-    //             <ReactMarkdown>{section.sectionBody}</ReactMarkdown>
-    //           </div>
-    //         </section>
-    //       ))}
-    //     </div>
-    //   </main>
-    //   <Footer />
-    // </div>
+    <div className="min-h-screen flex flex-col">
+      <AlertBar />
+      <Navbar />
+      <main className="flex-1 bg-white">
+        <div className="max-w-4xl mx-auto py-8 px-4 space-y-12">
+          <section>
+            <h1 className="text-3xl font-bold mb-4">{title}</h1>
+            <div className="mb-6 text-lg prose prose-lg">
+              <ReactMarkdown>{body}</ReactMarkdown>
+            </div>
+          </section>
+        </div>
+      </main>
+      <Footer />
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const actionPath = path.join(process.cwd(), 'content', 'action.md');
+  const actionContent = fs.readFileSync(actionPath, 'utf8');
+  const { data } = matter(actionContent);
+
+  return {
+    props: {
+      title: data.title,
+      body: data.body,
+    },
+  };
 }
