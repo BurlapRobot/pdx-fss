7af6d373763097d004379b773fbdb5cf4fd125fa
Kim Robinson <kimmykokonut@gmail.com>
2025-07-28 14:01:27 -0700

    [event-calendar] stub event calendar page and cms admin

---
+++ content/calendar.md
@@ -0,0 +1,4 @@
+---
+title: Event Calendar
+body: Stay up to date with upcoming events, meetings, and advocacy opportunities.
+---
--- pages/calendar.js
+++ pages/calendar.js
@@ -1,5 +1,40 @@
-export default function Calendar({}) {
-  return(
-    <div>Page TBD</div>
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+import ReactMarkdown from 'react-markdown';
+import AlertBar from "../components/AlertBar";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+export default function EventCalendar({ title, body }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <AlertBar />
+      <Navbar />
+      <main className="flex-1 bg-white">
+        <div className="max-w-4xl mx-auto py-8 px-4 space-y-12">
+          <section>
+            <h1 className="text-3xl font-bold mb-4">{title}</h1>
+            <div className="mb-6 text-lg prose prose-lg">
+              <ReactMarkdown>{body}</ReactMarkdown>
+            </div>
+          </section>
+        </div>
+      </main>
+      <Footer />
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const eventCalendarPath = path.join(process.cwd(), 'content', 'calendar.md');
+  const eventCalendarContent = fs.readFileSync(eventCalendarPath, 'utf8');
+  const { data } = matter(eventCalendarContent);
+
+  return {
+    props: {
+      title: data.title,
+      body: data.body,
+    },
+  };
 }
