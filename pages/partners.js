be02f58dfcfcdec89ffc67ad452cc85bb9f8378c
Kim Robinson <kimmykokonut@gmail.com>
2025-08-08 12:08:44 -0700

    [partners] add partners page and cms page

---
+++ pages/partners.js
@@ -0,0 +1,39 @@
+import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
+import ReactMarkdown from "react-markdown";
+
+export default function Partners({ title, body }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-1 bg-white">
+        <div className="max-w-4xl mx-auto py-8 px-4 space-y-12">
+          <section>
+            <h1 className="text-3xl font-bold mb-4">{title}</h1>
+            <div className="mb-6 text-lg prose prose-lg">
+              <ReactMarkdown>{body}</ReactMarkdown>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "content", "partners.md");
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  const { data: fileData } = matter(fileContent);
+
+  const navPath = path.join(process.cwd(), "content", "navbar.md");
+  const navContent = fs.readFileSync(navPath, "utf8");
+  const { data: navData } = matter(navContent);
+
+  return {
+    props: {
+      title: fileData.title,
+      body: fileData.body,
+      navbar: navData,
+    },
+  };
+}
