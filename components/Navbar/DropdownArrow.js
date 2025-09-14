9c21cece5d8b5e375b68a066f01382a114d3c809
Ka Hung Lee <lee.kahung414@gmail.com>
2025-09-14 00:20:39 -0700

    Replace custom svg with FSS asset for caret down

--- components/Navbar/DropdownArrow.js
+++ components/Navbar/DropdownArrow.js
@@ -1,38 +1,22 @@
+import Image from "next/image";
 import PropTypes from "prop-types";
 
 const DropdownArrow = ({ isExpanded }) => {
-  const caratStyles = isExpanded
-    ? "fill-black stroke-black"
-    : "group-hover:fill-black group-hover:stroke-black";
-
   return (
     <span
       className={`
-        flex h-[24px] w-[16px] ml-1
+        flex h-[24px] min-w-[16px] ml-1
         ${isExpanded ? "rotate-180" : "group-hover:rotate-180"}
         duration-200 transition-transform`}
     >
-      <svg
-        viewBox="0 0 100 50"
-        xmlns="http://www.w3.org/2000/svg"
-        role="img"
-        className={`
-        fill-[#FFCA08]
-        stroke-[#FFCA08]
-        ${caratStyles}`}
-      >
-        <polygon
-          points="12,8 88,8 50,42"
-          strokeLinejoin="round"
-          strokeWidth="6"
-        />
-        <polygon
-          points="12,8 88,8 50,42"
-          strokeLinejoin="round"
-          strokeLinecap="round"
-          strokeWidth="14"
-        />
-      </svg>
+      <Image
+        src="/images/FSS-assets/icon-caret-down.svg"
+        className={`group-hover:invert-0 group-hover:brightness-0
+          ${isExpanded ? "invert-0 brightness-0" : ""}`}
+        alt="icon x"
+        height={21}
+        width={14}
+      />
     </span>
   );
 };
