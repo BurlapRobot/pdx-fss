cdca9559326896d89113e0e0125d91321b49ddc2
Ka Hung Lee <lee.kahung414@gmail.com>
2025-09-24 13:59:13 -0700

    Include import for getStaticPropsData helper function for about page

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
 import { getCommonPageProps } from "../utils/getPageProps";
 import ContentPageLayout from "../components/ContentPageLayout";
+import getStaticPropsData from "../utils/getStaticPropsHelper";
 
 export default function About({ title, content }) {
   return <ContentPageLayout title={title} content={content} />;
