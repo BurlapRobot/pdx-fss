fefb1c0473d7102a0811288e19a716d951d98279
Ka Hung Lee <lee.kahung414@gmail.com>
2025-09-26 14:27:34 -0700

    Include rendering conditions for clickable links in alert bar

--- components/AlertBar.js
+++ components/AlertBar.js
@@ -14,13 +14,18 @@ const AlertBar = ({
     <div className="bg-primary_50 text-xs text-black px-4 py-2 flex justify-between items-center w-full">
       <span>
         {alertText}{" "}
-        <a href={linkUrl} className="underline">
+        <a
+          href={linkUrl}
+          className={`underline ${linkText.trim() === "" ? "hidden" : ""}`}
+        >
           {linkText}
         </a>
       </span>
       <a
         href={buttonUrl}
-        className="ml-4 bg-black text-primary_50 px-2 py-1 rounded text-xs hover:bg-gray-800 transition-colors"
+        className={`ml-4 bg-black text-primary_50 px-2 py-1 
+          rounded text-xs hover:bg-gray-800 transition-colors
+          ${buttonText.trim() === "" ? "hidden" : ""}`}
       >
         {buttonText}
       </a>
