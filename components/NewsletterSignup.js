59649c47a69e74ee3d9b4600d07d9a81b0f1ae13
Jacob Baqleh <baqlehjacob1@gmail.com>
2025-10-03 15:32:34 -0700

    add email sign up page

--- components/NewsletterSignup.js
+++ components/NewsletterSignup.js
@@ -1,9 +1,9 @@
-import StyledButton from "./shared/StyledButton";
+import StyledLink from "./shared/StyledLink";
 
 const NewsletterSignup = () => (
   <section
     className="bg-gray-100 border-t border-b border-dotted border-gray-400
-      py-4 px-4 flex flex-col items-center"
+      py-4 px-4 flex flex-col items-center "
   >
     <div className="max-w-[500px]">
       <p className="text-sm mb-2">
@@ -12,8 +12,8 @@ const NewsletterSignup = () => (
         <br />
         We anticipate one to two email messages per month.
       </p>
-      <form className="flex items-end text-sm space-x-2 w-full">
-        <fieldset className="w-full text-sm">
+      <form className="flex justify-center">
+        {/* <fieldset className="w-full text-sm">
           <legend className="mb-2 text-neutral_50 font-semibold">
             Your Email Address
           </legend>
@@ -22,8 +22,8 @@ const NewsletterSignup = () => (
             placeholder="email@email.com"
             className="px-2 py-1 h-8 md:h-9 border border-gray-300 rounded w-full"
           />
-        </fieldset>
-        <StyledButton type="submit">Submit</StyledButton>
+        </fieldset> */}
+        <StyledLink size='small' href='/email-signup'>Sign Up</StyledLink>
       </form>
     </div>
   </section>
